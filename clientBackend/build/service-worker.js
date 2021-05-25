86644ef4d0fde38ed1c30c6877bcc754b1a4b0aa
GhassenKs <jasonksouri@example.com>
2021-05-25 20:24:30 +0100

    production working

--- clientBackend/build/service-worker.js
+++ clientBackend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b1f1f140dd2a3bbd0a12d2d51b43f3c0.js"
+  "/precache-manifest.97feee8afb41d8ad975ac23a1020631a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
